05ee390cacdf78938cc847972c7bb697a017534d
lamPolar <lovast7@gmail.com>
2024-03-27 18:18:36 +0900

    feat : axios 로 api get 요청 #4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,33 @@
+"use client";
+import axios from "axios";
+import { useEffect } from "react";
+
 export default function Home() {
-  return <></>;
+  useEffect(() => {
+    axios.defaults.baseURL = process.env.NEXT_PUBLIC_API_URL;
+    // 200 성공
+    axios
+      .get(`/hello`, {
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Content-Type": "application/json",
+        },
+      })
+      .then((res) => alert(res.data));
+
+    // 403 에러 발생
+    axios
+      .get("/hi")
+      .then((res) => console.log(res.data))
+      .catch((err) => console.log(err));
+  }, []);
+  return (
+    <>
+      <header>
+        <div>Chat</div>
+        <div>Setting</div>
+      </header>
+      Hello ChatUp
+    </>
+  );
 }
